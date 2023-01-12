b04a3ef0c6a892831d04c96e5a4315ba4dc53c6a
shubhamshinde <ss9834ps@gmail.com>
2023-01-12 22:47:55 +0530

    first commit

---
+++ src/com/shubham/Test.java
@@ -0,0 +1,23 @@
+package com.shubham;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		list.add(40);
+		list.add(10);
+		list.add(50);
+		list.add(30);
+		list.add(60);
+		list.add(20);
+		
+		System.out.println(list);
+		
+	}
+
+}
